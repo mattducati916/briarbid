3ba0ff0dfa326d7d40ed7cead41ec371835598c9
mattducati916 <mattducati@gmail.com>
2026-04-02 10:38:10 -0400

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './lib/AuthContext'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Auctions from './pages/Auctions'
+import AuctionDetail from './pages/AuctionDetail'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Sell from './pages/Sell'
+import Watchlist from './pages/Watchlist'
+import Notifications from './pages/Notifications'
+import Profile from './pages/Profile'
+import NotFound from './pages/NotFound'
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/auctions" element={<Auctions />} />
+            <Route path="/auction/:id" element={<AuctionDetail />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/sell" element={<Sell />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
